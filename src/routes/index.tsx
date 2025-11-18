603fa37548aca50538273bfcd0056dae178c5b0c
Eliott Moreno <eliott_mor@hotmail.com>
2025-11-17 19:25:10 -0700

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -16,13 +16,13 @@ function RouteComponent() {
     <div className="relative isolate overflow-hidden bg-white dark:bg-gray-900 min-h-screen transition-colors duration-200">
       <svg
         aria-hidden="true"
-        className="absolute inset-0 -z-10 size-full mask-[radial-gradient(100%_100%_at_top_right,white,transparent)] stroke-gray-900/10 dark:stroke-white/10"
+        className="absolute inset-0 -z-10 size-full mask-[radial-gradient(100%_100%_at_top_right,white,transparent)] stroke-gray-200"
       >
         <defs>
           <pattern
             x="50%"
             y={-1}
-            id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+            id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
             width={200}
             height={200}
             patternUnits="userSpaceOnUse"
@@ -30,8 +30,26 @@ function RouteComponent() {
             <path d="M.5 200V.5H200" fill="none" />
           </pattern>
         </defs>
-        <rect fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)" width="100%" height="100%" strokeWidth={0} />
+        <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+          <path
+            d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+            strokeWidth={0}
+          />
+        </svg>
+        <rect fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)" width="100%" height="100%" strokeWidth={0} />
       </svg>
+      <div
+        aria-hidden="true"
+        className="absolute top-10 left-[calc(50%-4rem)] -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:top-[calc(50%-30rem)] lg:left-48 xl:left-[calc(50%-24rem)]"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
+          }}
+          className="aspect-1108/632 w-277 bg-linear-to-r from-[#bbff80] to-[#16a34a] opacity-20"
+        />
+      </div>
       <div className="mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-8 lg:py-40">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:shrink-0 lg:pt-8">
           <div className="mt-24 sm:mt-32 lg:mt-16">
