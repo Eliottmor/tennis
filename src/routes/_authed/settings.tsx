3f626a49b16fa6b01134968cc3591ba470bc2f2e
Eliott Moreno <eliott_mor@hotmail.com>
2025-11-22 00:02:13 -0700

    unused var

--- src/routes/_authed/settings.tsx
+++ src/routes/_authed/settings.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useMutation } from 'convex/react'
 import { api } from '../../../convex/_generated/api'
 import { Input } from '~/ui/input'
